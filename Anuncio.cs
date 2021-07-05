1c984d231067cacca60cd9f4760131e8c3e17759
Tatiane <49874215+tatianex@users.noreply.github.com>
2021-07-04 23:32:40 -0300

    Update Anuncio.cs

--- Anuncio.cs
+++ Anuncio.cs
@@ -8,6 +8,7 @@ public class Anuncio
 	DateTime DataTermino { get; set; }
 	decimal InvestimentoPorDia { get; set; }
 
+	// Construtor para criar anúncio, recebe nome, data de início e termino e investimento
 	public Anuncio(string nome, cliente Cliente, DateTime dataInicio, DateTime dataTermino, decimal investimentoPorDia)
 	{
 		this.Nome = nome;
@@ -17,18 +18,26 @@ public class Anuncio
 		this.InvestimentoPorDia = investimentoPorDia;
 	}
 
+	
+	// Relatório do valor total investido por cliente e data
 	valorTotalInvestido(cliente Cliente, dataInicio DateTime, dataTermino DateTime)
-    {
+    	{
 
-    }
+    	}
+	
+	// Relatório da quantidade máxima de visualizações por cliente e data
 	quantidadeMaximaDeVisualizacoes(cliente Cliente, dataInicio DateTime, dataTermino DateTime)
 	{
 
 	}
+	
+	// Relatório da quantidade máxima de cliques por cliente e data
 	quantidadeMaximaDeCliques(cliente Cliente, dataInicio DateTime, dataTermino DateTime)
 	{
 
 	}
+	
+	// Relatório da quantidade máxima de compartilhamentos por cliente e data
 	quantidadeMaximaDeCompartilhamentos(cliente Cliente, dataInicio DateTime, dataTermino DateTime)
 	{
 
